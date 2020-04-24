d7d13cfa3ea38341c0cba863705e2ab219e490c4
Yuriy Bevov <jury19y@mail.ru>
2020-04-24 21:01:43 +0300

    блок срочное производство + правки разметки в других блоках

--- source/js/scripts/swiperSliderInit.js
+++ source/js/scripts/swiperSliderInit.js
@@ -1,45 +1,74 @@
 'use strict';
 
 (function () {
-  const mySwiper = new Swiper ('.swiper-container', {
-    // Optional parameters
+  const isDealershipSlider = document.querySelector('.dealership-swiper-container');
 
-    // loop: true,
-    slidesPerView: 1,
-    centeredSlides: true,
-    grabCursor: true,
-    slideToClickedSlide: true,
-    initialSlide: 0,
+  if (isDealershipSlider) {
 
-    // Navigation arrows
+    const indexPageSwiper = new Swiper ('.dealership-swiper-container', {
+      // Optional parameters
 
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
+      // loop: true,
+      slidesPerView: 1,
+      centeredSlides: true,
+      grabCursor: true,
+      slideToClickedSlide: true,
+      initialSlide: 0,
 
-    breakpoints: {
-      // when window width is >= 480px
-      480: {
-        slidesPerView: 2,
-        spaceBetween: 30,
-        initialSlide: 2,
+      // Navigation arrows
+
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
       },
-      640: {
-        slidesPerView: 3,
-        spaceBetween: 40,
-        initialSlide: 2,
-      },
-      960: {
-        slidesPerView: 3,
-        spaceBetween: 50,
-        initialSlide: 2,
-      },
-      1200: {
-        slidesPerView: 5,
-        spaceBetween: 80,
-        initialSlide: 2,
+
+      breakpoints: {
+        // when window width is >= 480px
+        480: {
+          slidesPerView: 2,
+          spaceBetween: 30,
+          initialSlide: 2,
+        },
+        640: {
+          slidesPerView: 3,
+          spaceBetween: 40,
+          initialSlide: 2,
+        },
+        960: {
+          slidesPerView: 3,
+          spaceBetween: 50,
+          initialSlide: 2,
+        },
+        1200: {
+          slidesPerView: 5,
+          spaceBetween: 80,
+          initialSlide: 2,
+        }
       }
-    }
-  });
+    });
+  }
+
+  const isUrgentSlider = document.querySelector('.urgent-swiper-container');
+
+  if (isUrgentSlider) {
+    const urgentPageSwiper = new Swiper ('.urgent-swiper-container', {
+      slidesPerView: 1,
+
+      navigation: {
+        nextEl: '.urgent-swiper-button-next',
+        prevEl: '.urgent-swiper-button-prev',
+      },
+
+      breakpoints: {
+        // when window width is >= 836px
+        836: {
+          slidesPerView: 2,
+        },
+        // when window width is >= 1160px
+        1160: {
+          slidesPerView: 3,
+        }
+      }
+    });
+  }
 })();
