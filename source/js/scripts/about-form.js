212d86031dfb07c7149b3ae528bf4f92ce332d29
Yuriy Bevov <jury19y@mail.ru>
2020-04-29 20:59:36 +0300

    правка формы в хедере, проверка валидности и дерева

---
+++ source/js/scripts/about-form.js
@@ -0,0 +1,16 @@
+/*"use-strict";
+
+(function() {
+  const fileInput = document.getElementById('about-form-file-upload');
+  const fileName = document.querySelector('.about-form__upload-file');
+
+  if (fileInput) {
+    fileInput.addEventListener('change', function(event) {
+      const input = event.target;
+
+      for (let i = 0; i < input.files.length; i++) {
+        fileName.textContent = input.files[i].name;
+      }
+    });
+  }
+})();*/
