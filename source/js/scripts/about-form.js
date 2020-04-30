bb602788987feff387202bce62e229d3bfefb59a
Yuriy Bevov <jury19y@mail.ru>
2020-04-30 22:22:37 +0300

    правки для мозиллы

--- source/js/scripts/about-form.js
+++ source/js/scripts/about-form.js
@@ -1,16 +1,36 @@
 /*"use-strict";
 
 (function() {
-  const fileInput = document.getElementById('about-form-file-upload');
-  const fileName = document.querySelector('.about-form__upload-file');
+  const form = document.querySelector('.about__request form');
 
-  if (fileInput) {
-    fileInput.addEventListener('change', function(event) {
-      const input = event.target;
+  if(form) {
+    const mailInput = document.getElementById('about-form__email');
+console.log('form-validation')
+    let confirm = false
 
-      for (let i = 0; i < input.files.length; i++) {
-        fileName.textContent = input.files[i].name;
+    form.addEventListener('submit', (evt) => {
+      if(confirm) {
+        console.log('ok')
+        return
+      } else if(!confirm) {
+        evt.preventDefault();
+        console.log('error')
       }
-    });
+    })
+
+    form.addEventListener('change', () => {
+      if(validateEmail(mailInput.value)) {
+        mailInput.style.color = "rgb(0, 0, 0)"
+        confirm = true;
+      } else {
+        mailInput.style.color = "red"
+        confirm = false;
+      }
+    })
+
+    function validateEmail(email) {
+      var regexp = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      return regexp.test(String(email).toLowerCase());
+    }
   }
 })();*/
--- source/js/scripts/order-modal.js
+++ source/js/scripts/order-modal.js
@@ -52,4 +52,53 @@
       }
     });
   }
+
+  /*const countInput = document.getElementById('count');
+
+  if (countInput) {
+    const inputMask = IMask( countInput, {
+      mask: Number,  // enable number mask
+
+      // other options are optional with defaults below
+      scale: 2,  // digits after point, 0 for integers
+      signed: false,  // disallow negative
+      thousandsSeparator: ' ',  // any single char
+
+      // additional number interval options (e.g.)
+      min: 1,
+      max: 10000000
+    });
+  }*/
+
+  /*const form = document.querySelector('.order-modal form');
+
+  if(form) {
+    const mailInput = document.getElementById('mail');
+    const errorMsg = form.querySelector(".order-modal__mail-error");
+
+    let confirm = false
+
+    form.addEventListener('submit', (evt) => {
+      if(confirm) {
+        return
+      } else if(!confirm) {
+        evt.preventDefault();
+      }
+    })
+
+    form.addEventListener('change', () => {
+      if(validateEmail(mailInput.value)) {
+        errorMsg.style.display = "none";
+        confirm = true;
+      } else {
+        errorMsg.style.display = "block";
+        confirm = false;
+      }
+    })
+
+    function validateEmail(email) {
+      var regexp = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      return regexp.test(String(email).toLowerCase());
+    }
+  }*/
 })();
