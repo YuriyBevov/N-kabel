14cc9aa3e5fd04048f27cc52d544a35c9472938c
Yuriy Bevov <jury19y@mail.ru>
2020-05-06 19:11:18 +0300

    правка стилей в power-cable.html

--- source/js/scripts/swiperSliderInit.js
+++ source/js/scripts/swiperSliderInit.js
@@ -120,6 +120,10 @@
         el: '.swiper-pagination',
         dynamicBullets: true,
       },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
     });
   }
 })();
